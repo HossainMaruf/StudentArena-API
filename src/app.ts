2879f14f1fcbad0822a075fc5f12a50d97903e58
HossainMaruf <maruficepustian@gmail.com>
2025-12-11 20:47:33 +0600

    minor refactor of app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import userRoutes from './routes/userRoutes';
 import postRoutes from './routes/postRoutes';
 
 const app: Application = express();
+
+// Middleware
 app.use(express.json());
 
 // Initialize MySQL connection
@@ -11,6 +13,7 @@ AppDataSource.initialize()
  .then(() => console.log("Data Source Initialized"))
  .catch((err: any) => console.log("Error during Data Source Initialization: ", err));
 
+// Routes
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
